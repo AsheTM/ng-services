0478e6490a7091456807fbbe30d94bea8b3c3c0a
AsheTM <haithemalidaoud@gmail.com>
2023-01-28 14:48:23 +0100

    Init @ashetm/ng-utility Angular library

---
+++ projects/utility/src/lib/ng-utility.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgUtilityComponent } from './ng-utility.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgUtilityComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgUtilityComponent
+  ]
+})
+export class NgUtilityModule { }

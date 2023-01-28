f56ce0a43406952e0579745e8ac342783a3d2772
AsheTM <haithemalidaoud@gmail.com>
2023-01-28 14:48:23 +0100

    Init @ashetm/ng-google-auth Angular library

---
+++ projects/google-auth/src/lib/google-auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { GoogleAuthComponent } from './google-auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    GoogleAuthComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    GoogleAuthComponent
+  ]
+})
+export class GoogleAuthModule { }

c40236a773943e544c93776ae3e940c9e03d624e
AsheTM <haithemalidaoud@gmail.com>
2023-02-05 11:13:09 +0100

    Implement openai module and service

--- projects/openai/src/lib/openai.module.ts
+++ projects/openai/src/lib/openai.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
-import { OpenaiComponent } from './openai.component';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Configuration, OpenAIApi } from 'openai';
 
+import { OpenairootModule } from './openai-root.module';
+import { AOpenaiConfiguration } from './openai.class';
+import { OpenaiService } from './openai.service';
+import { OPENAI_TOKEN_CONFIGURATION } from './openai.token';
+import { TOpenaiConfiguration } from './openai.type';
 
 
 @NgModule({
   declarations: [
-    OpenaiComponent
   ],
   imports: [
   ],
   exports: [
-    OpenaiComponent
   ]
 })
-export class OpenaiModule { }
+export class OpenaiModule {
+
+  constructor() {
+    throw new Error('Import OpenaiModule.forRoot static method!');
+  }
+
+  static forRoot(configuration: TOpenaiConfiguration): ModuleWithProviders<OpenairootModule> {
+    return {
+      ngModule: OpenairootModule,
+      providers: [
+        {
+          provide: OpenaiService,
+          useFactory: (configuration: AOpenaiConfiguration) => new OpenAIApi(configuration),
+          deps: [OPENAI_TOKEN_CONFIGURATION]
+        }, {
+          provide: OPENAI_TOKEN_CONFIGURATION,
+          useValue: new Configuration(configuration)
+        }
+      ]
+    };
+  }
+
+}
--- projects/openai/src/public-api.ts
+++ projects/openai/src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of openai
  */
 
-export * from './lib/openai.service';
-export * from './lib/openai.component';
+export * from './lib/openai-root.module';
+export * from './lib/openai.class';
 export * from './lib/openai.module';
+export * from './lib/openai.service';
+export * from './lib/openai.token';
+export * from './lib/openai.type';

638f57961b9b2f0bea0c99f24d77a92f01b12794
Haitham DAWOUD <haitham_dawoud@ext.carrefour.com>
2025-01-12 16:40:19 +0100

    feat - init @ashetm/ng-network library

---
+++ projects/network/src/lib/network.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-network',
+  template: `
+    <p>
+      network works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NetworkComponent {
+
+}
---
+++ projects/network/src/lib/network.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NetworkComponent } from './network.component';
+
+
+
+@NgModule({
+  declarations: [
+    NetworkComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NetworkComponent
+  ]
+})
+export class NetworkModule { }
